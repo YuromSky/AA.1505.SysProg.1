68b8b54080605a45c2ed99a6ef40346dee1cd049
Александр Шишкин <avshsv01@gmail.com>
2018-05-18 16:58:09 +0300

    Update robots dll

---
+++ Robot (12)/Robot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using RobotContracts;
+
+namespace Robot
+{
+    public class Robot : IRobot
+    {
+        public string Name
+        {
+            get
+            {
+                return "Baseline Robot";
+            }
+        }
+
+        public RobotAction Tick(int robotId, RoundConfig config, GameState state)
+        {
+            RobotState self = state.robots[robotId];
+            RobotAction action = new RobotAction();
+
+            Random rng = new Random();
+            action.targetId = state.robots[robotId > 0 ? 0 : 1].id;
+            if (rng.Next(0, 2) > 0)
+                action.dX = 1;
+
+            if (rng.Next(0, 2) > 0)
+                action.dY = 1;
+
+            return action;
+        }
+    }
+}
