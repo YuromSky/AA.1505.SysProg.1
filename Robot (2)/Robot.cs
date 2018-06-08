b924a45625dee202a3c598713cad2fa2e16c80a9
vasserman1997 <34859835+vasserman1997@users.noreply.github.com>
2018-06-08 10:53:26 +0300

    Update Robot.cs

--- Robot (2)/Robot.cs
+++ Robot (2)/Robot.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using RobotContracts;
 
 namespace Robot
@@ -9,14 +9,15 @@ namespace Robot
         {
             get
             {
-                return "Baseline Robot";
+                return "Vasserman Bot";
             }
         }
+
         public int Colour
         {
             get
             {
-                return 0;
+                return 1; // pink
             }
         }
 
@@ -24,16 +25,69 @@ namespace Robot
         {
             RobotState self = state.robots[robotId];
             RobotAction action = new RobotAction();
-
-            Random rng = new Random();
             action.targetId = -1;
-            if (rng.Next(0, 2) > 0)
-                action.dX = 1;
 
-            if (rng.Next(0, 2) > 0)
-                action.dY = 1;
+            if (self.energy > 0 && self.isAlive)
+            {
+                // find nearest energy station
+                int pointId = 0;
+                int pointDist = config.height * config.width;
+                for (int ptId = 0; ptId < state.points.Count; ptId++)
+                {
+                    Point pt = state.points[ptId];
+                    if (pt.type == PointType.Energy)
+                    {
+                        int ptDist = CalcDistance(self.X, self.Y, pt.X, pt.Y);
+                        if (ptDist < pointDist)
+                        {
+                            pointDist = ptDist;
+                            pointId = ptId;
+                        }
+                    }
+                }
+
+                Point targetPoint = state.points[pointId];
+
+                int maxDistance = 10 * config.max_speed * self.speed / config.max_health * self.energy / config.max_energy;
+                if (pointDist <= maxDistance)
+                {
+                    action.dX = targetPoint.X - self.X;
+                    action.dY = targetPoint.Y - self.Y;
+                }
+                else
+                {
+                    int steps = pointDist / maxDistance + 1;
+                    action.dX = (targetPoint.X - self.X) / steps;
+                    action.dY = (targetPoint.Y - self.Y) / steps;
+                }
+
+                // defense
+                int targetId = -1;
+                int targetDist = config.width * config.height;
+                for (int rsId = 0; rsId < state.robots.Count; rsId++)
+                {
+                    RobotState rs = state.robots[rsId];
+                    int rsDist = CalcDistance(self.X, self.Y, rs.X, rs.Y);
+                    if (rs.name != self.name && rs.energy > 0 && rsDist < targetDist)
+                    {
+                        targetDist = rsDist;
+                        targetId = rsId;
+                    }
+                }
+
+                int maxDistanceAttack = 10 * config.max_radius * self.speed / config.max_health * self.energy / config.max_energy;
+                if (targetDist <= maxDistanceAttack)
+                {
+                    action.targetId = targetId;
+                }
+            }
 
             return action;
+        }
+
+        private int CalcDistance(int x1, int y1, int x2, int y2)
+        {
+            return (int)Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
         }
     }
 }
